3689479d6d1a61a5c89300fe8d4a70697af54f1a
qiji <qiji@sigmayun.com>
2019-12-12 14:47:45 +0800

    IU-2019.3 <qiji@LAPTOP Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, Vue Single File Component.vue, ide.general.xml, git.xml, web-types-registry.xml, ide-features-trainer.xml, common.xml, project.default.xml, rainbow_brackets.xml, diff.xml, Default copy.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, checkstyle-idea.xml, keymap.xml, plugin_ui.xml, colors.scheme.xml, KeyPromoterXSettings.xml, github.xml, mavenVersion.xml, templates.xml, Vue.xml, stringManipulation.xml, Default.xml, laf.xml, myGroup.xml, NightLion Aptana Theme.icls, parameter.hints.xml, .xml, jdk.table.xml, mykeymaps.xml, path.macros.xml, mir.icls, web-browsers.xml, wsl.distributions.xml, proxy.settings.xml, vcs.xml, databaseSettings.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Vue Single File Component.vue
@@ -0,0 +1,22 @@
+<!-- ${COMPONENT_NAME} -->
+<template>
+  <div>#[[$END$]]#</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {},
+  watch: {},
+  created() {}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
